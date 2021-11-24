e943934b14c250f689f67988069e914257a18eb0
Nikolai Arsov <niki_arsov@mail.bg>
2021-11-24 17:09:04 +0000

    setup admin panel

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //LaraAdmin panel
+        //Dwij\Laraadmin\LAProviderASDF::class
+
     ],
 
     /*
